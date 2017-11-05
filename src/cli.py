3de8e951badd4d31aaa18e087a73f464700ea952
Aleem Haji <hajial@gmail.com>
2017-11-05 14:49:31 -0500

    Rewrote everything to use a proper manager class, and a CLI class.

--- src/cli.py
+++ src/cli.py
@@ -1,12 +1,110 @@
 import argparse
+import os
+import sqlite3
 import sys
+from datetime import date
 
-from tasker import TaskerCli
+from tasker import Tasker, InvalidStartDateException, DuplicateNameException, InvalidCadenceException
+
+class Queries(object):
+    SELECT_INCOMPLETE_TIS = '''
+        SELECT ti.id, t.name, ti.date
+        FROM tasks t
+        JOIN tis ti ON ti.task = t.id
+        WHERE ti.done = "false";
+    '''
+
+class TaskCreator(object):
+    def __init__(self, db):
+        self.db = db
+
+    def _get_task_name(self):
+        tasker = Tasker(self.db)
+        cursor = self.db.cursor()
+
+        while True:
+            name = raw_input('Enter task name: ')
+            try:
+                tasker.assert_name_unique(name)
+                return name
+            except DuplicateNameException as e:
+                print >> sys.stderr, e.message
+
+    def _get_cadence(self):
+        while True:
+            print 'Available cadences:'
+            print '\n'.join('  {}. {}'.format(i+1, o.title()) for i, o in enumerate(Tasker.Cadence.ALL))
+            cadence = raw_input('Select cadence: ')
+            try:
+                cadence = Tasker.Cadence.ALL[int(cadence) - 1]
+            except ValueError:
+                pass
+
+            cadence = cadence.lower()
+            try:
+                Tasker(self.db).assert_cadence_valid(cadence)
+                return cadence
+            except InvalidCadenceException as e:
+                print >> sys.stderr, e.message
+
+    def _get_first_date(self):
+        while True:
+            start = raw_input('When does this start (YYYY-MM-DD): ')
+            try:
+                return date(*[int(i) for i in start.split('-')])
+            except:
+                print >> sys.stderr, 'Not a valid (YYYY-MM-DD)'
+
+    def create_task_from_user_input(self):
+        tasker = Tasker(self.db)
+        name = self._get_task_name()
+        cadence = self._get_cadence()
+        while True:
+            start = self._get_first_date()
+            try:
+                tasker.assert_start_date_valid(cadence, start)
+                break
+            except InvalidStartDateException as e:
+                print >> sys.stderr, e.message
+
+        Tasker(self.db).create_task(name, cadence, start)
 
 
-class TaskerCliCommand(object):
-    ADD_TASK = object()
-    RUN_CHECK = object()
+class TaskPrinter(object):
+    def __init__(self, db):
+        self.db = db
+
+    def print_remaining_tasks(self):
+        cursor = self.db.cursor()
+        cursor.execute(Queries.SELECT_INCOMPLETE_TIS)
+        self.db.commit()
+
+        task_instances = cursor.fetchall()
+        if len(task_instances):
+            print 'Things to do:'
+            for row in task_instances:
+                print '    {}. ({}) {}'.format(row[0], row[2], row[1])
+            print 'To complete any task, use:\n    {} --complete N'.format(sys.argv[0])
+
+
+class TaskerCli(object):
+    def __init__(self, database=None):
+        if not database:
+            database = os.path.join(os.path.expanduser('~'), '.tasker.sqlite')
+
+        self.db = sqlite3.connect(database)
+        self.tasker = Tasker(self.db)
+
+    def create_task(self):
+        TaskCreator(self.db).create_task_from_user_input()
+
+    def print_tasks(self):
+        self.tasker.schedule_tasks()
+        TaskPrinter(self.db).print_remaining_tasks()
+
+    def complete_task(self, ti_id):
+        self.tasker.complete_task_instance(ti_id)
+
 
 
 def do_program():
--- src/tasker.py
+++ src/tasker.py
@@ -4,6 +4,22 @@ import sys
 from datetime import date, timedelta
 
 
+class TaskerException(Exception):
+    pass
+
+
+class DuplicateNameException(TaskerException):
+    pass
+
+
+class InvalidStartDateException(TaskerException):
+    pass
+
+
+class InvalidCadenceException(TaskerException):
+    pass
+
+
 class Queries(object):
     CREATE_TASKS_TABLE = '''
         CREATE TABLE IF NOT EXISTS tasks (
@@ -20,12 +36,6 @@ class Queries(object):
             date DATE,
             done BOOLEAN DEFAULT false
         );
-    '''
-    SELECT_INCOMPLETE_TIS = '''
-        SELECT ti.id, t.name, ti.date
-        FROM tasks t
-        JOIN tis ti ON ti.task = t.id
-        WHERE ti.done = "false";
     '''
     SELECT_SCHEDULABLE_TASKS = '''
         SELECT t.id, t.name, t.cadence, t.start, ti.date, ti.done
@@ -45,105 +55,152 @@ class Queries(object):
     INSERT_TI = '''
         INSERT INTO tis (task, date) VALUES (?,?);
     '''
+    UPDATE_TI_DONE = '''
+        UPDATE tis SET done = "true" WHERE id = ?
+    '''
 
 
-class Cadence(object):
-    DAILY = 'daily'
-    WEEKLY = 'weekly'
-    MONTHLY = 'monthly'
+class Tasker(object):
+    """
+    Class that manages recurring tasks in an SQLite3 database.
+    """
+    class Cadence(object):
+        """
+        Pseudo-enum for supported cadences.
+        """
+        DAILY = 'daily'
+        WEEKLY = 'weekly'
+        MONTHLY = 'monthly'
 
-    ALL = (DAILY, WEEKLY, MONTHLY)
+        ALL = (DAILY, WEEKLY, MONTHLY)
 
+    def __init__(self, database):
+        """
+        :param database: The sqlite3 database connection for this instance.
+        """
+        self.db = database
+        self._db_initialized = False
 
-class TaskCreator(object):
-    def __init__(self, db):
-        self.db = db
+    def _initialize_db(self):
+        """
+        Initialize the database by ensuring that the required tables are present.
 
-    def _get_task_name(self):
-        cursor = self.db.cursor()
-
-        while True:
-            name = raw_input('Enter task name: ')
-
-            cursor.execute(Queries.SELECT_TASK_BY_NAME, (name,))
-            for row in cursor:
-                if row == (0,):
-                    return name
-                print >> sys.stderr, 'Task already exists.'
-
-    def _get_cadence(self):
-        while True:
-            print 'Available cadences:'
-            print '\n'.join('  {}. {}'.format(i+1, o.title()) for i, o in enumerate(Cadence.ALL))
-            cadence = raw_input('Select cadence: ')
-            try:
-                cadence = Cadence.ALL[int(cadence) - 1]
-            except ValueError:
-                pass
-
-            if cadence.lower() in Cadence.ALL:
-                return cadence
-
-    def _get_first_date(self):
-        while True:
-            start = raw_input('When does this start (YYYY-MM-DD): ')
-            try:
-                return date(*[int(i) for i in start.split('-')])
-            except:
-                print >> sys.stderr, 'Not a valid (YYYY-MM-DD)'
-
-    def create_task_from_user_input(self):
-        name = self._get_task_name()
-        cadence = self._get_cadence()
-        while True:
-            start = self._get_first_date()
-            if not (cadence == Cadence.MONTHLY and start.day > 28):
-                break
-            print >> sys.stderr, 'Cannot have monthly tasks scheduled after the 28th.'
+        NOTE: Currently has no support whatsoever for managing schema evolution.
+        """
+        if self._db_initialized:
+            return
 
         cursor = self.db.cursor()
-        cursor.execute(Queries.INSERT_TASK, (name, cadence, start))
+        cursor.execute(Queries.CREATE_TASKS_TABLE)
+        cursor.execute(Queries.CREATE_TASK_INSTANCES_TABLE)
         self.db.commit()
 
-
-class TaskUpdater(object):
-    def __init__(self, db):
-        self.db = db
+        self._db_initialized = True
 
     def _get_next_date(self, cadence, start_date, last_date):
+        """
+        Get the next scheduleable date for a given start date and last scheduled date.
+
+        :param cadence: Schedule cadence for the task being checked.
+        :param start_date: The date the initial task was scheduled.
+        :param last_date: The last date a task instance was scheduled.
+        """
         if not last_date:
             return start_date
 
         date_components = last_date.split('-')
         recent_date = date(*(int(d) for d in date_components))
-        if cadence == Cadence.DAILY:
+        if cadence == Tasker.Cadence.DAILY:
             return (recent_date + timedelta(days=1)).isoformat()
-        if cadence == Cadence.WEEKLY:
+        if cadence == Tasker.Cadence.WEEKLY:
             return (recent_date + timedelta(days=7)).isoformat()
-        elif cadence == Cadence.MONTHLY:
+        elif cadence == Tasker.Cadence.MONTHLY:
             # No "month" timedelta :(
             if recent_date.month == 12:
                 return (recent_date.replace(year=recent_date.year + 1, month=1)).isoformat()
             return (recent_date.replace(month=recent_date.month + 1)).isoformat()
 
-    def update_task_instances(self):
+    def assert_cadence_valid(self, cadence):
+        """
+        Ensure that the provided cadence exists in the set of supported cadences.
+
+        :raises 
+        """
+        if cadence not in Tasker.Cadence.ALL:
+            raise InvalidCadenceException('Cadence {} not available.')
+
+
+    def assert_name_unique(self, name):
+        """
+        Ensure that a task by this name doesn't already exist in the tasks list.
+
+        :raises DuplicateNameException: When a task with this name already exists.
+        """
+        self._initialize_db()
+
+        cursor = self.db.cursor()
+        cursor.execute(Queries.SELECT_TASK_BY_NAME, (name,))
+        self.db.commit()
+
+        for row in cursor:
+            if row != (0,):
+                raise DuplicateNameException('Task "{}" already exists'.format(name))
+
+    def assert_start_date_valid(self, cadence, start_date):
+        """
+        Ensure that the date provided makes sense for the cadence it should run at.
+
+        :raises InvalidStartDateException: When a start date and cadence could cause tasks to skip instances.
+        """
+        if cadence == Tasker.Cadence.MONTHLY and start_date.day > 28:
+            raise InvalidStartDateException(
+                'Cadence {} and start date: {} could lose task instances.'.format(cadence, start_date)
+            )
+
+    def create_task(self, name, cadence, start_date):
+        """
+        Create a task that will be used to derive task instances.
+
+        :param name: The name of the task.
+        :param cadence: How often this task will schedule itself to create task instances.
+        :param start_date: The date for which the first task instance should create itself.
+        """
+        self.assert_cadence_valid(cadence)
+        self.assert_start_date_valid(cadence, start_date)
+
+        self._initialize_db()
+        self.assert_name_unique(name)
+        cursor = self.db.cursor()
+        cursor.execute(Queries.INSERT_TASK, (name, cadence, start_date))
+        self.db.commit()
+
+    def schedule_tasks(self, until_date=None):
+        """
+        Search through the list of all tasks, and ensure that if a task could be scheduled on or before today's date,
+        that it exists in the database with the earliest of possible dates. i.e. The next task instance should be 
+        scheduled if it's not in the future.
+        """
         cursor = self.db.cursor()
         cursor.execute(Queries.SELECT_SCHEDULABLE_TASKS)
         self.db.commit()
 
-        today_date = date.today().strftime('%Y-%m-%d')
+        if not until_date:
+            until_date = date.today()
+
+        until_date_str = until_date.strftime('%Y-%m-%d')
         insert_statements = []
 
+        # This could probably be cleaned up a lot, likely by fixing the query, more than anything.
         for row in cursor:
             # Three possible cases.
             #    - The most recent ti is done. (Check date and maybe create a new one)
             #    - The most recent ti is not done. (Leave it)
             #    - There has never been a ti. (Make one)
             next_date = self._get_next_date(row[2], row[3], row[4])
-            if next_date == row[4] or next_date > today_date:
+            if next_date == row[4] or next_date > until_date_str:
                 continue
 
-            if next_date <= today_date and row[5] != 'false':
+            if next_date <= until_date_str and row[5] != 'false':
                 insert_statements.append((row[0], next_date))
 
         cursor = self.db.cursor()
@@ -151,48 +208,11 @@ class TaskUpdater(object):
         self.db.commit()
 
     def complete_task_instance(self, ti_id):
+        """
+        Set the provided task instance to be "done"
+
+        :param ti_id: The id for the task instance.
+        """
         cursor = self.db.cursor()
-        cursor.execute('UPDATE tis SET done = "true" WHERE id = ?', (ti_id,))
+        cursor.execute(Queries.UPDATE_TI_DONE, (ti_id,))
         self.db.commit()
-
-
-class TaskPrinter(object):
-    def __init__(self, db):
-        self.db = db
-
-    def print_remaining_tasks(self):
-        cursor = self.db.cursor()
-        cursor.execute(Queries.SELECT_INCOMPLETE_TIS)
-        self.db.commit()
-
-        task_instances = cursor.fetchall()
-        if len(task_instances):
-            print 'Things to do:'
-            for row in task_instances:
-                print '    {}. ({}) {}'.format(row[0], row[2], row[1])
-            print 'To complete any task, use:\n    {} --complete N'.format(sys.argv[0])
-
-
-class TaskerCli(object):
-    def __init__(self, database=None):
-        if not database:
-            database = os.path.join(os.path.expanduser('~'), '.tasker.sqlite')
-
-        self.db = sqlite3.connect(database)
-        self._init_db()
-
-    def _init_db(self):
-        cursor = self.db.cursor()
-        cursor.execute(Queries.CREATE_TASKS_TABLE)
-        cursor.execute(Queries.CREATE_TASK_INSTANCES_TABLE)
-        self.db.commit()
-
-    def create_task(self):
-        TaskCreator(self.db).create_task_from_user_input()
-
-    def print_tasks(self):
-        TaskUpdater(self.db).update_task_instances()
-        TaskPrinter(self.db).print_remaining_tasks()
-
-    def complete_task(self, ti_id):
-        TaskUpdater(self.db).complete_task_instance(ti_id)
