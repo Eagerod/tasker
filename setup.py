7aa3cf7a8d622b0e04ce8f2593228cc08abea8f4
Aleem Haji <hajial@gmail.com>
2017-12-17 22:04:42 -0500

    Version bump, invalidates old sqlite databases because of type changes.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name='tasker',
     license='MIT',
-    version='0.1.4',
+    version='0.2.0',
     description='manage recurring tasks on the command line.',
     author='Aleem Haji',
     author_email='hajial@gmail.com',
