d3a0272987afde03492c1b47b0e4c5c340cc7b2f
Aleem Haji <hajial@gmail.com>
2017-12-21 23:26:29 -0500

    Updated models to support column lengths, as MySQL requires.

--- src/models/task.py
+++ src/models/task.py
@@ -8,6 +8,6 @@ class Task(Base):
     __tablename__ = 'tasks'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    cadence = Column(String, nullable=False)
+    name = Column(String(1024), nullable=False)
+    cadence = Column(String(256), nullable=False)
     start = Column(Date, nullable=False)
