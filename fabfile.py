1d44e25b5afb35443554637b45fb725a1a8d710a
Aleem Haji <hajial@gmail.com>
2017-12-23 11:51:04 -0500

    Set up fabfile to not run commands more than once if chaining commands.

--- fabfile.py
+++ fabfile.py
@@ -1,29 +1,33 @@
 import os
 
-from fabric.api import local, task
+from fabric.api import local, task, runs_once
 
 
 PROJECT_ROOT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 
 @task
+@runs_once
 def setup(quiet=False):
     local('pip install {} -r requirements.txt'.format('--quiet' if quiet else ''))
 
 
 @task
+@runs_once
 def lint():
     setup(quiet=True)
     local('flake8 .')
 
 
 @task
+@runs_once
 def test():
     setup(quiet=True)
     local('python -m unittest discover -v -t . -s tests')
 
 
 @task
+@runs_once
 def coverage():
     setup(quiet=True)
 
@@ -48,6 +52,7 @@ def coverage():
 
 
 @task
+@runs_once
 def install_globally(extras=''):
     if extras:
         extras = '[{}]'.format(extras)
